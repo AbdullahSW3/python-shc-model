d7deee862103631125a90afb8f287efc21bd07b5
FaisalDH <109041188+FaisalDH@users.noreply.github.com>
2023-05-26 09:17:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
-from flask import Flask, jsonify, request, session
+from sklearn import metrics
+from flask import Flask, jsonify, request
 import json
 import joblib
 import warnings
@@ -11,6 +13,8 @@ import secrets
 warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+user_responses = {}
 
 @app.route('/name', methods=["GET", "POST"])
 def nameRoute():
@@ -21,18 +25,29 @@ def nameRoute():
         sym = np.array(name)
         sym = np.array([sym])
 
-        # Model Accuracy, how often is the classifier correct?
-        # print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-        # print(cross_val_score(rnd_forest,X_train_data,y_train_data,cv=5).mean())
         with open('Model.joblib', 'rb') as f:
             predictor = joblib.load(f)
+        
         value = predictor.predict(sym)
         prob = predictor.predict_proba(sym)
         class_labels = predictor.classes_
         disease = value[0]
         class_probabilities = dict(zip(class_labels, prob[0]))
-        response = class_probabilities
-        return jsonify({'name': response})
+        
+        # Generate a unique ID for the user
+        user_id = secrets.token_hex(16)
+        user_responses[user_id] = class_probabilities
+        
+        return jsonify({'user_id': user_id})
+    
+    else:
+        user_id = request.args.get('user_id')
+        
+        if user_id in user_responses:
+            response = user_responses[user_id]
+            return jsonify({'name': response})
+        else:
+            return jsonify({'error': 'Invalid user_id'})
 
 if __name__ == "__main__":
     app.run(debug=False)
